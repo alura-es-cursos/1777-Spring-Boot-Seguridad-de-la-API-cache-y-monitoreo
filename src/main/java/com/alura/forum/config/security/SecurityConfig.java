b99c320bfd05b1f38ec61902940df9ca40049ca4
Icaro Florencio <icaro.florencio@mercadolivre.com>
2021-02-15 18:20:46 -0300

    aula 6

--- src/main/java/com/alura/forum/config/security/SecurityConfig.java
+++ src/main/java/com/alura/forum/config/security/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 					.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 					.antMatchers(HttpMethod.POST, "/auth").permitAll()
+					.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 					.anyRequest().authenticated()
 					.and().csrf().disable()
 					.sessionManagement()
